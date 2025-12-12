e094de3fdea04bc0e04d80c6ab1f9708076eab42
pmathys <you@example.com>
2025-12-12 17:09:33 +0100

    feat(handler404, error404, router) : updated 404 page and deploying 404 page for every invalid route

--- internal/http/handler404.go
+++ internal/http/handler404.go
@@ -6,12 +6,23 @@ import (
 	"github.com/YajiTV/groupie-tracker/internal/templates"
 )
 
+type Error404Data struct {
+	Title  string
+	Path   string
+	Method string
+}
+
 func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusNotFound)
 
-	if err := templates.Templates.ExecuteTemplate(w, "error404.gohtml", map[string]any{
-		"Title": "404 — Page introuvable",
-	}); err != nil {
+	data := Error404Data{
+		Title:  "404 — Page introuvable",
+		Path:   r.URL.Path,
+		Method: r.Method,
+	}
+
+	if err := templates.Templates.ExecuteTemplate(w, "error404.gohtml", data); err != nil {
 		http.Error(w, "404 — Page introuvable", http.StatusNotFound)
 	}
 }
